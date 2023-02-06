b3c31ce39a722534838a149821b895392a09b078
Simon Paul <samonpaul06@gmail.com>
2023-02-06 16:56:53 +0530

    create test

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react"
 import AdminTestBlock from "../components/AdminTestBlock"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import Loader from "../components/Loader"
 
-
-const Admin = ({ tests }) => {
+const Admin = ({ tests, newTest, setNewTest, showLoader, setShowLoader }) => {
 
   const [currentChapter, setCurrenChapter] = useState("1")
   const [data, setData] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const [show, setShow] = useState(false);
+
+
+  const navigate = useNavigate()
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -18,12 +21,23 @@ const Admin = ({ tests }) => {
   }, [currentChapter, tests])
 
   useEffect(() => {
-    console.log(data)
     setIsLoading(false)
   }, [data])
 
+  // handle new test
+  const handleNewTest = (e) => {
+    setNewTest({...newTest, [e.target.name]: parseInt(e.target.value)})
+  }
+
+  // handle create 
+  const handleCreate = () => {
+    handleClose()
+    navigate('/admin/createTest')
+  }
+
   return (
     <div className="container mx-auto">
+      {showLoader && <Loader />}
 
       {/* create test modal */}
 
@@ -34,8 +48,6 @@ const Admin = ({ tests }) => {
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 
-
-
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
 
               <div className="bg-gray-50 px-4 py-3">
@@ -43,27 +55,24 @@ const Admin = ({ tests }) => {
               </div>
 
               <div className="py-2 px-6 mt-4">
-                <div class="grid gap-6 mb-6 md:grid-cols-3">
+                <div className="grid gap-6 mb-6 md:grid-cols-3">
                   <div>
-                    <label for="first_name" class="block mb-2 text-sm font-medium">Chapter Number</label>
-                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <label htmlFor="chapterName" className="block mb-2 text-sm font-medium">Chapter Number</label>
+                    <input type="number" id="chapterName" className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" name="chapter" value={newTest.chapter} onChange={handleNewTest} required />
                   </div>
                   <div>
-                    <label for="last_name" class="block mb-2 text-sm font-medium">Test Number</label>
-                    <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <label htmlFor="testNumber" className="block mb-2 text-sm font-medium">Test Number</label>
+                    <input type="number" id="testNumber" className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" name="testNum" value={newTest.testNum} onChange={handleNewTest} required />
                   </div>
                   <div>
-                    <label for="company" class="block mb-2 text-sm font-medium">Time Limit</label>
-                    <input type="text" id="company" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <label htmlFor="timelimit" className="block mb-2 text-sm font-medium">Time Limit</label>
+                    <input type="number" id="timelimit" className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" name="timelimit" value={newTest.timelimit} onChange={handleNewTest} required />
                   </div>
                 </div>
-                <div class="mb-6">
-                  <label for="email" class="block mb-2 text-sm font-medium">Email address</label>
-                  <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
-                </div>
+
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" onClick={handleClose}>Deactivate</button>
+                <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" onClick={handleCreate}>Create</button>
                 <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={handleClose}>Cancel</button>
               </div>
             </div>
